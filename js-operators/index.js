fe54fc5505944a44c4fb4185a0ed7ded110c0753
Daniel Ojediran <tundeojediran@gmail.com>
2023-02-13 13:29:11 +0100

    add js-operators files

---
+++ js-operators/index.js
@@ -0,0 +1,78 @@
+
+
+
+// result.innerText = 'Result here...'
+
+
+// Arithmetic Operations
+let a = 100;
+let b = 45;
+
+let sum = a + b;
+// let sumElement = document.getElementById('sum');
+// sumElement.innerText = sum;
+console.log(sum);
+
+let difference = a - b;
+// let differenceElement = document.getElementById('difference');
+// differenceElement.innerText = difference;
+console.log(difference);
+
+let multiplication = a * b;
+// let multiplicationElement = document.getElementById('multiplication');
+// multiplicationElement.innerText = multiplication;
+console.log(multiplication);
+
+let division = a / b;
+// let divisionElement = document.getElementById('division');
+// divisionElement.innerText = division;
+console.log(division)
+
+
+// MODULO OPERATOR
+let c = 50;
+let remainder = a % c;
+console.log('Remainder of a divided by c equals ' + remainder);
+
+// EXPONENTIAL OPERATOR **
+let exponentResult = c ** 3;
+console.log('exponent result equals ' + exponentResult);
+
+// INCREMENT (++) & DECREMENT (--) OPERATORS
+// ++  is the same thing as  + 1;
+// -- is the same thing as - 1;
+let initialValue = 43;
+initialValue++; // same thing as initialValue = initialValue + 1;
+
+console.log('value is ' + initialValue);
+
+let age = 30;
+age--;
+console.log('New age value is ' + age);
+
+let numberOfCars  = 66;
+console.log('number of cars is ' +  ++numberOfCars);
+
+let numberOfOranges  = 99;
+console.log('I now have ' + numberOfOranges++ + ' oranges')
+
+++numberOfOranges // 100 => (1 + numberOfOranges)
+numberOfOranges++ // 99; add 1 in the next variable call 
+
+// ASSIGNMENT OPERATION
+let x = x + 1; // is same as let x++
+let y = y - 1; // is same as let y--
+
+let z = z + 6 // let z += 6;
+let w = w - 6 // let w -= 6;
+let p = p * 6 // let p *= 6;
+let q = q / 6 // let q /= 6;
+
+// COMPARISON OPERATOR (>, <, >=, <=, , !=, !==, ==, ===)
+let d = -34;
+console.log(d > 10);
+
+
+
+
+
