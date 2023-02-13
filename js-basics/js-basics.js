3529f887ea2066c27e25b486e53e0299d760ad24
Daniel Ojediran <tundeojediran@gmail.com>
2023-02-13 13:28:53 +0100

    add js-basics files

---
+++ js-basics/js-basics.js
@@ -0,0 +1,12 @@
+console.log('Stutern Cohort 1.4');
+
+
+document.getElementById("wrappper").innerHTML = "<p>Lily Nnabeze</p>";
+document.getElementById("name").innerText = "Joyce Adindu";
+
+// function that gets difference between two numbers
+function subtract (number1, number2) {
+    return number1 - number2;
+}
+
+console.log(subtract(20, 5));
---
+++ js-basics/variables.js
@@ -0,0 +1,61 @@
+
+// My name is Daniel. I am a Frontend Engineer. I live in Lagos, Nigeria
+
+// Variable names can be constructed using the following:
+// Letters A-Z , a-z,
+// Underscore _ , 
+// Numbers 0-9
+
+// let name_2;
+// let NAME;
+
+// NAME is not equal to name is not equals to Name
+
+let firstName = 'Daniel';
+let profession = 'Frontend Engineer';
+let location = 'Lagos, Nigeria';
+
+
+// console.log(firstName);
+// console.log(profession);
+// console.log(location);
+
+
+// CONCATENATION
+// console.log("My name is " + firstName + ". I am a " + profession + ". I live in " + location + "." )
+
+let age = 20;
+// age = "Daniel";
+// console.log("I am " + age + " years old.")
+
+// Dynamically / Weakly typed language
+
+console.log("54" + 5);
+// console.log(73 + "5");
+// console.log("65" - 6);
+console.log("34" + "6");
+
+// let
+// const
+// var - almost out of phase 
+
+
+/**
+ * there are 8 datatypes in JavaScript
+ * - Number
+ * - String
+ * - Boolean (true/false)
+ * - null
+ * - undefined
+ * - BigInt
+ * - Symbol
+ * - Object
+ * - Arrays
+ *  */ 
+
+
+let color = "red";
+
+let vehicle = "Honda";
+vehicle = "Toyota";
+
